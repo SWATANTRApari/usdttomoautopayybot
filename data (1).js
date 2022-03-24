d7ab5176c6bce7870f324920cc1d803533daf8cc
SWATANTRApari <102210380+SWATANTRApari@users.noreply.github.com>
2022-03-24 21:58:45 +0530

    Update data (1).js

--- data (1).js
+++ data (1).js
@@ -1,1 +1,25 @@
-module.exports = {mongoLink:'mongodb+srv://radical:vishu@radical.bvk17.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database linkbot_token:'5146627550:AAFmkxteswgBO8gUvWlzrySNInvwjekHOJo',bot_name:'Tomo_autopaybot',//bot username without @bot_admin:1811781327,channelsList : ['clickgonews'],channelscheck : ['@clickgonews'],airdropName : 'Digital Airdrop Co.',reffer_bonus:300,// ral bonus amountmin_wd:10,//min withdrawal amountcurrency:'rdt', //bot cuurencypayment_channel:'@clickgonews'// payment channel username}
+module.exports = {
+
+mongoLink:'mongodb+srv://Swatantra:Swatantra@cluster0.8dt9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'.b//database link
+
+bot_token:'5101040746:AAHr3uatK3Wbbn3ClyBDzNGxF5UQbM9xOkU',
+
+bot_name:'UsdttomocoinAutopaybot',//bot username without @
+
+bot_admin:1844321348,
+
+channelsList : ['ATTITUDEARNERS'],
+
+channelscheck : ['@ATTITUDEARNERS'],
+
+airdropName : 'Digital Airdrop Co.',
+
+reffer_bonus:300,// ral bonus amount
+
+min_wd:10,//min withdrawal amount
+
+currency:'rdt', //bot cuurency
+
+payment_channel:'@payouts2022'// payment channel username
+
+}
